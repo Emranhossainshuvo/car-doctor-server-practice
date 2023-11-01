59b8b46414d67ca5e4d2a91dfff83fb7250f8d65
Shuvo <shuvoorba28@gmail.com>
2023-10-31 23:31:16 -0700

    my bookings routes has been added to the client side and bookings data from database bia server side has loadded

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
 // middleware
 app.use(cors());
@@ -11,8 +11,7 @@ app.use(express.json());
 
 // console.log(process.env.DB_PASS)
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +26,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client
+      .db("carRepair")
+      .collection("repairServices");
+
+    const bookingCollection = client.db("carRepair").collection("bookings");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, img: 1, price: 1, service_id: 1 },
+      };
+
+      // bookings
+
+      app.get('/bookings', async(req, res) => {
+        console.log(req.query.email)
+        let query = {}
+        if(req.query?.email){
+          query = {email: req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray(); 
+        res.send(result)
+      })
+
+      app.post('/bookings', async(req, res) => {
+        const booking = req.body; 
+        console.log(booking)
+        const result = await bookingCollection.insertOne(booking)
+        res.send(result)
+      })
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +78,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
